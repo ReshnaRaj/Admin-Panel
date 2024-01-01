f8ae620ba48a67c06d66fa00fd9a0fb444244490
ReshnaRaj <reshnarajan84@gmail.com>
2024-01-01 19:50:48 +0530

    updating

--- Admin-dashboard/src/App.jsx
+++ Admin-dashboard/src/App.jsx
@@ -1,27 +1,23 @@
 import { useState } from "react";
 
- 
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 
 function App() {
   return (
-    
-     <>
-     <Header/>
-   
-     <Sidebar/>
-
-     
- 
-     
-
-    
+    <>
+   <div className="flex">
+   <div className={`w-64 h-screen`}>
+    <Sidebar/>
+   </div>
+    <div className="p-7 text-2xl font-semibold flex-1 h-screen">
+      <Header/>
+    </div>
+    </div>
       
-     
-
-     </>
-    
+ 
+   
+    </>
   );
 }
 
--- Admin-dashboard/src/Sidebar.jsx
+++ Admin-dashboard/src/Sidebar.jsx
@@ -1,60 +1,49 @@
 import React from 'react'
-import { MdDashboard ,MdContactSupport } from "react-icons/md";
+import { MdDashboard, MdContactSupport } from "react-icons/md";
 import { GiFlowerPot } from "react-icons/gi";
 import { PiSimCard } from "react-icons/pi";
 import { IoLogoWechat } from "react-icons/io5";
+import { AiOutlineLogout } from "react-icons/ai";
 
 const Sidebar = () => {
   return (
-    <div className="flex lg:flex-row h-screen">
-    <div className="w-16 lg:w-40 bg-green-700 text-white">
-      <div className="p-4 flex items-center">
-        <img
-          className="w-14 rounded-full"
-          src="https://res.cloudinary.com/dsyln8j3g/image/upload/v1687947401/xmaxf464kqpy6vd0uhhm.webp"
-          alt="icon"
-        />
-         
-      </div>
+    <div className="flex flex-col lg:flex-row h-screen">
+      <div className="lg:w-60 bg-green-700 text-white">
+        <div className="p-4 flex flex-col items-center lg:items-start">
+          <img
+            className="w-20 h-20 lg:rounded-full"
+            src="https://res.cloudinary.com/dsyln8j3g/image/upload/v1687947401/xmaxf464kqpy6vd0uhhm.webp"
+            alt="icon"
+          />
+          <div className="text-center lg:text-left mt-2">
+            <p className="text-sm font-medium">John Doe </p>
+            <p className="text-xs">johndoe@example.com</p>
+          </div>
+        </div>
 
-      <div className="p-3 space-y-10">
-        <li className="flex items-center">
-          <span className="lg:ml-2 font-bold hidden lg:block ">
-            <MdDashboard/>Dashboard
-      
-          </span>
-        </li>
-        <li className="flex items-center">
-          <span className="lg:ml-2 font-bold hidden lg:block "> 
-          <GiFlowerPot />
-          Perks</span>
-        </li>
-        <li className="flex items-center">
-          <span className="lg:ml-2 font-bold hidden lg:block ">
-            <PiSimCard/>
-            AddOns
-          </span>
-        </li>
-        <li className="flex items-center">
-          <span className="lg:ml-2 font-bold hidden lg:block  "><IoLogoWechat />FAQ</span>
-        </li>
-        <li className="flex items-center">
-          <span className="lg:ml-2 font-bold hidden lg:block  "><MdContactSupport /> Support</span>
-        </li>
-        
-         
-         
-        
-       
-        
-       
+        <div className="p-3 space-y-4 lg:space-y-10">
+          <button className="flex items-center lg:hover:bg-blue-500 py-2 px-4 lg:rounded">
+            <MdDashboard className="mr-2" /> Dashboard
+          </button>
+          <button className="flex items-center lg:hover:bg-blue-500 py-2 px-4 lg:rounded">
+            <GiFlowerPot className="mr-2" /> Perks
+          </button>
+          <button className="flex items-center lg:hover:bg-blue-500 py-2 px-4 lg:rounded">
+            <PiSimCard className="mr-2" /> AddOns
+          </button>
+          <button className="flex items-center lg:hover:bg-blue-500 py-2 px-4 lg:rounded">
+            <IoLogoWechat className="mr-2" /> FAQ
+          </button>
+          <button className="flex items-center lg:hover:bg-blue-500 py-2 px-4 lg:rounded">
+            <MdContactSupport className="mr-2" /> Support
+          </button>
+          <button className="flex items-center lg:hover:bg-blue-500 py-2 px-4 lg:rounded">
+            Logout <AiOutlineLogout className="ml-2" />
+          </button>
+        </div>
       </div>
-       
-     
-     
     </div>
-  </div>
   )
 }
 
-export default Sidebar
+export default Sidebar;
