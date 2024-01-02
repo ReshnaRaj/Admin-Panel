9ba7b4c0369898831b4696b6ed26d42bede3df31
ReshnaRaj <reshnarajan84@gmail.com>
2024-01-02 11:36:18 +0530

    header

--- Admin-dashboard/src/Header.jsx
+++ Admin-dashboard/src/Header.jsx
@@ -4,23 +4,15 @@ const Header = () => {
   return (
     <>
       <header className="text-white flex">
-        {/* Left column (3/4 width) */}
         <div className="flex-3 bg-white shadow-lg">
-          {/* Content for the left column */}
           <img
             src="https://res.cloudinary.com/dsyln8j3g/image/upload/v1704173040/Pngtree_initial_p_circle_logo_vector_5143865_eryrdq.png"
             className="w-16 mr-24 ml-24"
             alt="Logo"
           />
-         
         </div>
-        {/* Right column (1/4 width) */}
-        <div className="flex-1 bg-blue-100 shadow-lg">
-          
-          {/* Content for the right column */}
-          {/* Example content */}
-          
-        </div>
+
+        <div className="flex-1 bg-blue-100 shadow-lg"></div>
       </header>
     </>
   );
--- Admin-dashboard/src/Home.jsx
+++ Admin-dashboard/src/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Container from './Container'
 import Container2 from './Container2';
-import Header from './Header';
+ 
 
 const Home = () => {
 
